7d27a05c1a677bf873f2678b91ee0e2aa9eb942f
lap-lik <lap-lik@yandex.ru>
2024-09-17 00:10:23 +0300

    feat: add exception

--- src/main/java/ru/kokhno/menuservice/storage/model/Category.java
+++ src/main/java/ru/kokhno/menuservice/storage/model/Category.java
@@ -1,7 +1,27 @@
 package ru.kokhno.menuservice.storage.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import ru.kokhno.menuservice.exception.MenuServiceException;
+
+@Slf4j
 public enum Category {
     BREAKFAST,
     LUNCH,
-    DINNER
+    DINNER;
+
+    @JsonCreator
+    public static Category fromString(final String category) {
+        try {
+            return Category.valueOf(category.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            String message = "Failed to convert a category from a string: " + category;
+            log.info(message);
+            throw MenuServiceException.builder()
+                    .message(message)
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+    }
 }
